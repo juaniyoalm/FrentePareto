a06a693d6b0cf38df9e792c0b16f60c31a408c62
Juan <juan.perez@neurodigital.es>
2018-03-12 18:40:34 +0100

    Inicio del proyecto

---
+++ src/main/java/FrentePareto/FrentePareto/App.java
@@ -0,0 +1,13 @@
+package FrentePareto.FrentePareto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/FrentePareto/FrentePareto/EntradaSalida.java
@@ -0,0 +1,5 @@
+package FrentePareto.FrentePareto;
+
+public class EntradaSalida {
+
+}
