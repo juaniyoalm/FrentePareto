5a2c3cd49c9b70fd451844a20668035bb198fb3a
Juan <juan.perez@neurodigital.es>
2018-03-20 19:19:10 +0100

    Frente de Pareto V1

--- src/main/java/FrentePareto/FrentePareto/EntradaSalida.java
+++ src/main/java/FrentePareto/FrentePareto/EntradaSalida.java
@@ -1,8 +1,59 @@
 package FrentePareto.FrentePareto;
 
-public class EntradaSalida {
-	int numero = 0;
-	int patata = 1;
-	int yanocompartolapatata = 2;
-}
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.TreeMap;
 
+public class EntradaSalida {
+
+	public EntradaSalida() {
+
+	}
+
+	public TreeMap<Integer, String[]> readFile() {
+		
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		TreeMap<Integer, String[]> salida = new TreeMap<Integer, String[]>();
+		String[] separador;
+		int count = 0;
+
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			archivo = new File("src/main/java/FrentePareto/FrentePareto/archivo.txt");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			// Lectura del fichero
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				System.out.println(linea);
+				separador = linea.split(" ");
+				salida.put(count, separador);
+				count++;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return salida;
+
+	}
+}
---
+++ src/main/java/FrentePareto/FrentePareto/Pincipal.java
@@ -0,0 +1,38 @@
+package FrentePareto.FrentePareto;
+
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class Pincipal {
+	
+	private static TreeMap<Integer, Integer> fPareto = new TreeMap<Integer, Integer>();
+	
+
+	public static void main(String[] args) {
+		
+		EntradaSalida io = new EntradaSalida();
+		TreeMap<Integer, String[]> data = io.readFile();
+		
+		getFrentePareto(data);
+		
+		System.out.println(fPareto.toString());
+	}
+
+	private static void getFrentePareto(TreeMap<Integer, String[]> data) {
+		
+		for(Entry<Integer, String[]> entry : data.entrySet()) {
+			  //Integer key = entry.getKey();
+			String[] value = entry.getValue();
+			int clave = Integer.valueOf(value[1]);
+			int valor = Integer.valueOf(value[0]);
+			  
+			if (!fPareto.containsKey(clave)) {
+				fPareto.put(clave, valor);
+			} else {
+				if (fPareto.get(clave) < Integer.valueOf(valor)) {
+					fPareto.put(clave, valor);
+				}
+			}
+		}
+	}
+}
